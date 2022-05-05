9854a66662bd19e2ffbe549c2aaa56a3369d6cea
wayne <wei.zheng02@weimob.com>
2022-05-05 10:53:18 +0800

    test: dev

--- init.js
+++ init.js
@@ -4,6 +4,6 @@ const fse = require('fs-extra');
 
 const vvDocPath = path.resolve(process.cwd(), '.vvDoc')
 
-rimraf(vvDocPath)
+rimraf(vvDocPath, () => {})
 
 fse.copySync(path.resolve(__dirname, 'template'), vvDocPath, { overwrite: true })
