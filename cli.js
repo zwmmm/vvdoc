c4888fd060fb436b6a0a02d6d5f1ea4e8d416c23
wayne <wei.zheng02@weimob.com>
2022-05-05 11:16:58 +0800

    test: dev

--- cli.js
+++ cli.js
@@ -1,11 +1,16 @@
 const { createServer, build } = require('vite');
 const path = require('path')
 const mode = process.argv[2] || 'dev';
+const rimraf = require('rimraf')
+const fse = require('fs-extra');
 
 const root = path.resolve(process.cwd(), '.vvDoc')
 
 ;(async () => {
-  if (mode === 'dev') {
+  if (mode === 'init') {
+    rimraf(root, () => {})
+    fse.copySync(path.resolve(__dirname, 'template'), root, { overwrite: true })
+  } else if (mode === 'dev') {
     const server = await createServer({
       root
     })
--- init.js
+++
@@ -1,9 +0,0 @@
-const rimraf = require('rimraf')
-const path = require('path')
-const fse = require('fs-extra');
-
-const vvDocPath = path.resolve(process.cwd(), '.vvDoc')
-
-rimraf(vvDocPath, () => {})
-
-fse.copySync(path.resolve(__dirname, 'template'), vvDocPath, { overwrite: true })
